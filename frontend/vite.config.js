fb1f79dc3da5a510143c068afcb4ca9df4414694
varunsaini28 <vs1536375@gmail.com>
2026-03-19 20:49:16 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,29 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()],
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://ims-gatewayserver.onrender.com',
+        changeOrigin: true,
+        secure: true
+      }
+    }
+  }
 })
